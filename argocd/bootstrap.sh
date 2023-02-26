c5283697a0ec41ddd8620e159101a46ee0c0dccd
kimfy <kim@wwan.no>
2023-02-26 20:33:57 +0100

    Remove GH Action for ArgoCD Apps. It is now on the bootstrap step

--- argocd/bootstrap.sh
+++ argocd/bootstrap.sh
@@ -4,10 +4,9 @@
 ## Deploy ArgoCD to a fresh Kubernetes cluster
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl port-forward svc/argocd-server -n argocd 8080:443 &
+kubectl delete port-forward svc/argocd-server
 
-# Deploy the first application
 SECRET=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d)
-argocd login "localhost:8080" --insecure
-argocd app create guestbook --repo https://github.com/argoproj/argocd-example-apps.git --path guestbook --dest-server https://kubernetes.default.svc --dest-namespace default
-argocd sync guestbook
+argocd login local --insecure --username admin --password $SECRET --port-forward --port-forward-namespace argocd
+argocd appset create apps.yaml --port-forward --port-forward-namespace argocd
+
