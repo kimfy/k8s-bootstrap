bbc41b074afe553c5b58ac612b66ec11d79d6de5
kimfy <kim@wwan.no>
2023-02-27 13:48:38 +0100

    update argocd to 2.6.2. Add cert-manager values from own repo

--- argocd/bootstrap.sh
+++ argocd/bootstrap.sh
@@ -3,7 +3,7 @@
 ## In this guide we will be installing ArgoCD using kubectl and GitHub Actions.
 ## Deploy ArgoCD to a fresh Kubernetes cluster
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/2.6.2/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.6.2/manifests/install.yaml
 
 SECRET=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d)
 argocd login local --insecure --username admin --password $SECRET --port-forward --port-forward-namespace argocd
