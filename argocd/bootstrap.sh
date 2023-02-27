c7775d6f2fa26ee2af84b927a91445b7feaed861
kimfy <kim@wwan.no>
2023-02-27 13:05:43 +0100

    Update to 2.6.2 to get external helm value functionality

--- argocd/bootstrap.sh
+++ argocd/bootstrap.sh
@@ -3,8 +3,7 @@
 ## In this guide we will be installing ArgoCD using kubectl and GitHub Actions.
 ## Deploy ArgoCD to a fresh Kubernetes cluster
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl delete port-forward svc/argocd-server
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/2.6.2/manifests/install.yaml
 
 SECRET=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d)
 argocd login local --insecure --username admin --password $SECRET --port-forward --port-forward-namespace argocd
