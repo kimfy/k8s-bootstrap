5501dc339373f3eab142ef730bd05829019f1949
kimfy <kim@wwan.no>
2023-02-27 12:06:56 +0100

    add --upsert flag to argocd

--- argocd/bootstrap.sh
+++ argocd/bootstrap.sh
@@ -8,5 +8,5 @@ kubectl delete port-forward svc/argocd-server
 
 SECRET=$(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d)
 argocd login local --insecure --username admin --password $SECRET --port-forward --port-forward-namespace argocd
-argocd appset create apps.yaml --port-forward --port-forward-namespace argocd
+argocd appset create apps.yaml --port-forward --port-forward-namespace argocd --upsert
 
